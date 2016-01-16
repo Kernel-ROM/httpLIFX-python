e3d55e235d4766783519e2b00ee3ebccc83a8e65
Ben Leonard-Lagarde <leonardlagardeb@gmail.com>
2016-01-16 19:49:27 +0000

    Toggle function bugfix

--- httpLIFX.py
+++ httpLIFX.py
@@ -140,6 +140,9 @@ def power(option):
 			else:
 				print "Powering off..."
 				makeRequest("lights/all/toggle", "POST")
+	# Use this option to toggle the power
+	elif option == 2:
+		makeRequest("lights/all/toggle", "POST")
 	return
 
 # Initiates LIFX
