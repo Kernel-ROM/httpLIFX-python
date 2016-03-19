acfb7bfc9576270403908d4daddde5f556ed72d8
Ben Leonard-Lagarde <leonardlagardeb@gmail.com>
2016-03-19 22:30:38 +0000

    Added ability to control individual and multiple bulbs

--- httpLIFX.py
+++ httpLIFX.py
@@ -6,29 +6,29 @@ import re
 import argparse
 
 
-def changeBrightness(value):
+def changeBrightness(value, bulb):
     """Change brightness as a percentage."""
     value = clamp(value, 1, 100)
     print "Changing brightness to: %s%%" % value
     value = value / float(100)
     data = {"brightness": value}
-    makeRequest("lights/all/state", "PUT", data)
+    makeRequest("lights/%s/state" % (bulb), "PUT", data)
     return
 
 
-def changeColour(colour, delay):
+def changeColour(colour, delay, bulb):
     """Validate colour using LIFX API then change it."""
     print "Changing colour to: %s" % colour
     data = {
         "color": colour,
         "duration": delay
     }
-    makeRequest("lights/all/state", "PUT", data)
+    makeRequest("lights/%s/state" % (bulb), "PUT", data)
     time.sleep(delay)
     return
 
 
-def changeKelvin(value, delay):
+def changeKelvin(value, delay, bulb):
     """Change warmth of bulb in degrees kelvin."""
     value = clamp(int(value), 2500, 9000)
     print "Changing kelvin to: %sk" % value
@@ -36,14 +36,14 @@ def changeKelvin(value, delay):
         'color': "kelvin:" + str(value),
         "duration": delay
     }
-    makeRequest("lights/all/state", "PUT", data)
+    makeRequest("lights/%s/state" % (bulb), "PUT", data)
     time.sleep(delay)
     return
 
 
-def checkOnline():
+def checkOnline(bulb):
     """Check if the bulb is online."""
-    response = getStatus()
+    response = getStatus(bulb)
     if response.status_code != 200:
         print "Connection Error!"
         return False
@@ -65,52 +65,67 @@ def commands():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
         description='Connect to the LIFX HTTP API & control your bulb over the internet.',
-        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=45))
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=50))
 
+    parser.add_argument('-s', '--select', default="all",
+                        help='Select which bulb to control, defaults to "all"')
     parser.add_argument('-p', '--power',
                         help='Valid choices are ON/OFF/TOGGLE')
     parser.add_argument('-c', '--colour',
-                        help='Change colour of bulb(s).')
+                        help='Change colour of bulb(s)')
     parser.add_argument('-t', '--toggle',
-                        help='Toggle power state.', action='store_true')
+                        help='Toggle power state', action='store_true')
     parser.add_argument('-b', '--brightness',
-                        help='Brightness value as a percentage.', type=int)
+                        help='Brightness value as a percentage', type=int)
     parser.add_argument('-k', '--kelvin',
-                        help='Warmth value between 2500 & 9000.', type=int)
+                        help='Warmth value between 2500 & 9000', type=int)
 
     args = parser.parse_args()
+    runargs(args)
+    return
+
+
+def runargs(args):
+    """Run users selected arguments."""
+    if args.select == "all":
+        bulb = args.select
+    else:
+        bulb = "label:%s" % args.select
+
+    if not checkOnline(bulb):
+        sys.exit(1)
 
     if args.power:
         if args.power.upper() == "ON":
-            power(1)
+            power(1, bulb)
         elif args.power.upper() == "OFF":
-            power(0)
+            power(0, bulb)
         elif args.power.upper() == "TOGGLE":
-            powerToggle()
+            powerToggle(bulb)
         else:
             print "Unknown command!"
 
     if args.toggle:
-        powerToggle()
+        powerToggle(bulb)
 
     if args.colour:
         v = makeRequest("color?string=" + args.colour, "GET")
         if v.status_code == 200:
-            changeColour(args.colour, 2)
+            changeColour(args.colour, 2, bulb)
         elif v.status_code == 422:
             print "Unknown Colour!"
 
     if args.brightness:
-        changeBrightness(args.brightness)
+        changeBrightness(args.brightness, bulb)
 
     if args.kelvin:
-        changeKelvin(args.kelvin, 1)
+        changeKelvin(args.kelvin, 1, bulb)
     return
 
 
-def getStatus():
+def getStatus(bulb):
     """Return the status of the bulb."""
-    response = makeRequest('lights/all', "GET")
+    response = makeRequest('lights/%s' % (bulb), "GET")
     return response
 
 
@@ -166,9 +181,9 @@ def makeRequest(url, Rtype, data=""):
     return response
 
 
-def power(option):
+def power(option, bulb):
     """Change bulb power state with validation."""
-    status = getStatus()
+    status = getStatus(bulb)
     q = status.json()
     if option == 1:
         for i in q:
@@ -176,30 +191,29 @@ def power(option):
                 print "Bulb is already on!"
             else:
                 print "Powering on..."
-                makeRequest("lights/all/toggle", "POST")
+                makeRequest("lights/%s/toggle" % (bulb), "POST")
+
     elif option == 0:
         for i in q:
             if i['power'] == 'off':
                 print "Bulb is already off!"
             else:
                 print "Powering off..."
-                makeRequest("lights/all/toggle", "POST")
+                makeRequest("lights/%s/toggle" % (bulb), "POST")
     return
 
 
-def powerToggle():
+def powerToggle(bulb):
     """Toggle bulb power state."""
     print "Toggling power..."
-    makeRequest("lights/all/toggle", "POST")
+    makeRequest("lights/%s/toggle" % (bulb), "POST")
     return
 
 
 def main():
     """Initiate LIFX."""
-    if checkOnline():
-        commands()
-    else:
-        sys.exit(1)
+    commands()
+    return
 
 if __name__ == "__main__":
     main()
